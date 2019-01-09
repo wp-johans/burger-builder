56ef341355d992aa7956c98b18a256337166d6cc
Johan Svensson <johans@webprefer.com>
2019-01-09 23:16:35 +0100

    Added Layout component.

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Auxi from '../../hoc/Auxi';
+
+const layout = (props) => (
+    <Auxi>
+        <div>Toolbar, SideDrawer, Backdrop</div>
+        <main>
+            {props.children}
+        </main>
+    </Auxi>
+);
+
+export default layout;
