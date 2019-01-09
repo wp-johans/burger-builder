86bb408d991ad13893f5a7400917e0f1563b8361
Johan Svensson <johans@webprefer.com>
2019-01-10 00:58:44 +0100

    Added some burger components but stupid css import not working..

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Auxi from '../../hoc/Auxi';
+import Classes from './Layout.css';
 
 const layout = (props) => (
     <Auxi>
         <div>Toolbar, SideDrawer, Backdrop</div>
-        <main>
+        <main className={Classes.Content}>
             {props.children}
         </main>
     </Auxi>
