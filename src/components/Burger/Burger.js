bc1afd23e6526c7ace95466a72e33246798b6643
Johan Svensson <johans@webprefer.com>
2019-03-08 14:28:06 +0100

    Builder controls

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
-import Classes from './Burger.css';
+import './Burger.css';
 
 const burger = (props) => {
+    let transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+        return [...Array(props.ingredients[igKey])].map((_, i) => {
+            return <BurgerIngredient key={igKey + i} type={igKey}/>;
+        });
+    })
+    .reduce((arr, el) => {
+        return arr.concat(el);
+    }, []);
+    
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients</p>
+    }
+
     return (
-        <div className={Classes.Burger}>
+        <div className='Burger'>
             <BurgerIngredient type='bread-top' />
-            <BurgerIngredient type='cheese' />
-            <BurgerIngredient type='meat' />
+            {transformedIngredients}
             <BurgerIngredient type='bread-bottom' />
         </div>
     );
--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import './BurgerIngredient.css';
+// import Styles from './BurgerIngredient.css'
 
 class BurgerIngredient extends Component {
     render() {
@@ -14,22 +15,22 @@ class BurgerIngredient extends Component {
             case ('bread-top'):
                 ingredient = (
                     <div className='BreadTop'>
-                        <div className={BurgerIngredient.Seeds1}></div>
-                        <div className={BurgerIngredient.Seeds2}></div>
+                        <div className='Seeds1'></div>
+                        <div className='Seeds2'></div>
                     </div>
                 );
                 break;
             case ('meat'):
-                ingredient = <div className={BurgerIngredient.Meat}></div>;
+                ingredient = <div className='Meat'></div>;
                 break;
             case ('cheese'):
-                ingredient = <div className={BurgerIngredient.Cheese}></div>;
+                ingredient = <div className='Cheese'></div>;
                 break;
             case ('salad'):
-                ingredient = <div className={BurgerIngredient.Salad}></div>;
+                ingredient = <div className='Salad'></div>;
                 break;
             case ('bacon'):
-                ingredient = <div className={BurgerIngredient.Bacon}></div>;
+                ingredient = <div className='Bacon'></div>;
                 break;
             default:
                 ingredient = null;
